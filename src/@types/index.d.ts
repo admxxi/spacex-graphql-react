289ef213768e5021dd72e7eea31baf4b71e1ed54
Andre Monteiro <admxxi@gmail.com>
2021-03-29 00:36:38 +0100

    [feature/components] @types

---
+++ src/@types/index.d.ts
@@ -0,0 +1,27 @@
+/* eslint-disable */
+export interface IPost {
+  id: number
+  title: string
+  body: string
+}
+
+export type Country = {
+  code: !number
+  name: !string
+  emoji: !string
+}
+
+export type Mission = {
+  mission_name: string
+  // id: number
+  // name: string
+}
+// export type Mission = {
+//   description: string
+//   id: ID
+//   manufacturers: [string]
+//   name: string
+//   twitter: string
+//   website: string
+//   wikipedia: string
+// }
