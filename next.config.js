39f8252fd5d56b9f000c0ad98b924bcbd2a5fb33
Andre Monteiro <admxxi@gmail.com>
2021-03-29 01:00:10 +0100

    [feature/components] Package Installation + Upgrades

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-const withTypescript = require('@zeit/next-typescript')
-
-module.exports = withTypescript({
-  webpack(config, options) {
-    return config
-  }
-})
-
-
 module.exports = {
   webpack(config) {
     config.module.rules.push({
